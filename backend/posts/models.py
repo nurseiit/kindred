b58b10c67dbbd7d5bc386b336681aa588a412315
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-20 22:56:16 +0600

    Tweak

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -7,5 +7,8 @@ class Post(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    title = (models.CharField(max_length=50),)
+    title = models.CharField(max_length=50)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
