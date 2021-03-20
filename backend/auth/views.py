26f6696e3fce869dedc3fa5e236eda2f5c9f9cc9
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-20 17:28:53 +0600

    Add login & register API

---
+++ backend/auth/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import RegisterSerializer
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
--- backend/kindred/urls.py
+++ backend/kindred/urls.py
@@ -27,7 +27,6 @@ router.register(r"groups", views.GroupViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/auth/", include("auth.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
