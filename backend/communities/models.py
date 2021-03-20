177d566155ebd1c7ee6a9480a5b1ae463d7af259
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-21 02:42:02 +0600

    Create api for communities

--- backend/communities/models.py
+++ backend/communities/models.py
@@ -8,12 +8,16 @@ from django.db import models
 class Community(TimeStampModel):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="communities")
     number_of_users = models.IntegerField(default=0)
     invite_code = models.UUIDField(default=uuid.uuid4)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.number_of_users = self.users.all().count()
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "communities"
