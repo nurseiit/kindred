14d2cf1ce5f8607d433b7da55be1a79b72fa3620
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-21 05:08:35 +0600

    Create events

---
+++ backend/events/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "start",
+        "end",
+    )
