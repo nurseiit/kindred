b302df6dd4cf4b4b05dee1a424fb83e7eaef9e3c
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2021-03-21 03:56:19 +0600

    chore: remove counter slice

--- frontend/src/app/rootReducer.ts
+++ frontend/src/app/rootReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import counter from '../features/counter/counterSlice';
 import auth from '../features/auth/authSlice';
 import user from '../features/user/userSlice';
 
 const rootReducer = combineReducers({
-  counter,
   auth,
   user,
 });
