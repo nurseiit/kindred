86c96511d8d51fa0a8d587b35c8ba264b2e14472
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-20 21:44:20 +0600

    Finish prod settings

--- backend/kindred/urls.py
+++ backend/kindred/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users import views
 
 router = routers.DefaultRouter()
@@ -26,7 +27,10 @@ router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
-    path("api/auth/", include("auth.urls")),
+    path("", include(router.urls)),
+    path("auth/", include("auth.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
