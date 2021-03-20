edb4a802ca62e2e6ba28ed77685284effa454c80
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2021-03-20 14:56:19 +0600

    feat: configure styled-components

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,17 @@
+import styled from 'styled-components';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { decrement, increment } from '../features/counter/counterSlice';
+
+const Span = styled.span`
+  color: violet;
+`;
 
 export default function Index() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
   return (
     <div>
-      <span>{count}</span>
+      <Span>{count}</Span>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
     </div>
