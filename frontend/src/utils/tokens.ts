db7c2be5b37e052bea0874f30cfef2f5d4ef1396
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2021-03-21 11:17:18 +0600

    fix: runtime server error on SSR

--- frontend/src/utils/tokens.ts
+++ frontend/src/utils/tokens.ts
@@ -14,19 +14,30 @@ export const saveTokens = (tokens: ITokens) => {
   const myDate = new Date();
   myDate.setMonth(myDate.getMonth() + MONTHS_TO_STORE_A_TOKEN_COOKIE);
 
-  Cookies.set(TOKEN_COOKIE_NAME, JSON.stringify(tokens), {
-    expires: myDate,
-    domain: COOKIE_DOMAIN,
-    secure: true,
-  });
+  try {
+    Cookies.set(TOKEN_COOKIE_NAME, JSON.stringify(tokens), {
+      expires: myDate,
+      domain: COOKIE_DOMAIN,
+      secure: true,
+    });
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export const getTokens = (): ITokens => {
   try {
-    return JSON.parse(Cookies.get(TOKEN_COOKIE_NAME) || '');
-  } catch (_) {
+    return JSON.parse(Cookies.get(TOKEN_COOKIE_NAME) || '{}');
+  } catch (e) {
+    console.error(e);
     return {};
   }
 };
 
-export const removeTokens = () => Cookies.remove(TOKEN_COOKIE_NAME);
+export const removeTokens = () => {
+  try {
+    Cookies.remove(TOKEN_COOKIE_NAME);
+  } catch (e) {
+    console.error(e);
+  }
+};
